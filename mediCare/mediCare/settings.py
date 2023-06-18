b5a794ee7dfbabd781c2eeba4ada584bf6e3683c
AjithMthomas <ajithmt46958@gmail.com>
2023-06-18 21:32:00 +0530

    changes in chat

--- mediCare/mediCare/settings.py
+++ mediCare/mediCare/settings.py
@@ -230,5 +230,5 @@ MEDIA_URL = '/media/'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_HOST_USER='ajithmuzhayamkattil@gmail.com'
-EMAIL_HOST_PASSWORD='idacvunegqkegldh'
+EMAIL_HOST_PASSWORD='lzrdvxthvszuhqre'
 EMAIL_USE_TLS=True
